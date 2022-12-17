c4594ac950bd3369a761d449848951dd1dc86c1c
Ignacio Albistu <profiles.albistu@gmail.com>
2022-12-17 13:39:28 -0500

    added global variables in JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,10 @@
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for 
 //that city and that city is added to the search history
-
+// Global Variables
+var searchSection = document.getElementById("searchSection");
+var previousCities = document.getElementById("previousCities");
+var weatherResults = document.getElementById("weatherResults");
 
 
 // WHEN I view current weather conditions for that city
